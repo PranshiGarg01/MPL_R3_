baf92ce40e21c2a7c31f94721535ba997466ae75
Pranshi <garg.pranshi09590@gmail.com>
2026-09-18 01:04:13 +0530

    Paths sorted

--- js/config.js
+++ js/config.js
@@ -1,10 +1,93 @@
 const HUNT_CONFIG = {
   path1: {
-    clue1: { answers: ["volleyball court"], next: "clue2.html", hint: "Proceed to Clue 02 location." },
-    clue2: { answers: ["nescafe"], next: "clue3.html", hint: "Proceed to Clue 03 location." },
-    clue3: { answers: ["library"], next: "clue4.html", hint: "Proceed to Clue 04 location." },
-    clue4: { answers: ["anna audi"], next: "clue5.html", hint: "Proceed to final clue!" },
-    clue5: { answers: ["cs hall"], next: "../success.html", hint: "Path 1 Cleared!" }
+    clue1: { answers: ["charmander"], next: "clue2.html", hint: "Proceed to Clue 02 location." },
+    clue2: { answers: ["bulbasaur"], next: "clue3.html", hint: "Proceed to Clue 03 location." },
+    clue3: { answers: ["pidgey"], next: "clue4.html", hint: "Proceed to Clue 04 location." },
+    clue4: { answers: ["weedie"], next: "clue5.html", hint: "Proceed to Clue 05 location." },
+    clue5: { answers: ["eevee"], next: "clue6.html", hint: "Proceed to Clue 06 location." },
+    clue6: { answers: ["moltres"], next: "clue7.html", hint: "Proceed to Clue 07 location." },
+    clue7: { answers: ["chikorita"], next: "../success.html", hint: "Path 1 Cleared!" }
+  },
+  path2: {
+    clue1: { answers: ["pikachu"], next: "clue2.html", hint: "Proceed to Clue 02 location." },
+    clue2: { answers: ["charmander"], next: "clue3.html", hint: "Proceed to Clue 03 location." },
+    clue3: { answers: ["pidgey"], next: "clue4.html", hint: "Proceed to Clue 04 location." },
+    clue4: { answers: ["caterpie"], next: "clue5.html", hint: "Proceed to Clue 05 location." },
+    clue5: { answers: ["weedie"], next: "clue6.html", hint: "Proceed to Clue 06 location." },
+    clue6: { answers: ["moltres"], next: "clue7.html", hint: "Proceed to Clue 07 location." },
+    clue7: { answers: ["chikorita"], next: "../success.html", hint: "Path 1 Cleared!" }
+  },
+  path3: {
+    clue1: { answers: ["charmander"], next: "clue2.html", hint: "Proceed to Clue 02 location." },
+    clue2: { answers: ["squlrtle"], next: "clue3.html", hint: "Proceed to Clue 03 location." },
+    clue3: { answers: ["bulbasaur"], next: "clue4.html", hint: "Proceed to Clue 04 location." },
+    clue4: { answers: ["pidgey"], next: "clue5.html", hint: "Proceed to Clue 05 location." },
+    clue5: { answers: ["weedie"], next: "clue6.html", hint: "Proceed to Clue 06 location." },
+    clue6: { answers: ["rattata"], next: "clue7.html", hint: "Proceed to Clue 07 location." },
+    clue7: { answers: ["moltres"], next: "../success.html", hint: "Path 1 Cleared!" }
+  },
+  path4: {
+    clue1: { answers: ["pikachu"], next: "clue2.html", hint: "Proceed to Clue 02 location." },
+    clue2: { answers: ["caterpie"], next: "clue3.html", hint: "Proceed to Clue 03 location." },
+    clue3: { answers: ["weedie"], next: "clue4.html", hint: "Proceed to Clue 04 location." },
+    clue4: { answers: ["rattata"], next: "clue5.html", hint: "Proceed to Clue 05 location." },
+    clue5: { answers: ["eevee"], next: "clue6.html", hint: "Proceed to Clue 06 location." },
+    clue6: { answers: ["moltres"], next: "clue7.html", hint: "Proceed to Clue 07 location." },
+    clue7: { answers: ["chikorita"], next: "../success.html", hint: "Path 1 Cleared!" }
+  },
+  path5: {
+    clue1: { answers: ["squlrtle"], next: "clue2.html", hint: "Proceed to Clue 02 location." },
+    clue2: { answers: ["bulbasaur"], next: "clue3.html", hint: "Proceed to Clue 03 location." },
+    clue3: { answers: ["pidgey"], next: "clue4.html", hint: "Proceed to Clue 04 location." },
+    clue4: { answers: ["weedie"], next: "clue5.html", hint: "Proceed to Clue 05 location." },
+    clue5: { answers: ["rattata"], next: "clue6.html", hint: "Proceed to Clue 06 location." },
+    clue6: { answers: ["butterfree"], next: "clue7.html", hint: "Proceed to Clue 07 location." },
+    clue7: { answers: ["chikorita"], next: "../success.html", hint: "Path 1 Cleared!" }
+  },
+  path6: {
+    clue1: { answers: ["pikachu"], next: "clue2.html", hint: "Proceed to Clue 02 location." },
+    clue2: { answers: ["squlrtle"], next: "clue3.html", hint: "Proceed to Clue 03 location." },
+    clue3: { answers: ["bulbasaur"], next: "clue4.html", hint: "Proceed to Clue 04 location." },
+    clue4: { answers: ["pidgey"], next: "clue5.html", hint: "Proceed to Clue 05 location." },
+    clue5: { answers: ["butterfree"], next: "clue6.html", hint: "Proceed to Clue 06 location." },
+    clue6: { answers: ["eevee"], next: "clue7.html", hint: "Proceed to Clue 07 location." },
+    clue7: { answers: ["moltres"], next: "../success.html", hint: "Path 1 Cleared!" }
+  },
+  path7: {
+    clue1: { answers: ["pikachu"], next: "clue2.html", hint: "Proceed to Clue 02 location." },
+    clue2: { answers: ["charmander"], next: "clue3.html", hint: "Proceed to Clue 03 location." },
+    clue3: { answers: ["squlrtle"], next: "clue4.html", hint: "Proceed to Clue 04 location." },
+    clue4: { answers: ["caterpie"], next: "clue5.html", hint: "Proceed to Clue 05 location." },
+    clue5: { answers: ["weedie"], next: "clue6.html", hint: "Proceed to Clue 06 location." },
+    clue6: { answers: ["rattata"], next: "clue7.html", hint: "Proceed to Clue 07 location." },
+    clue7: { answers: ["butterfree"], next: "../success.html", hint: "Path 1 Cleared!" }
+  },
+  path8: {
+    clue1: { answers: ["squlrtle"], next: "clue2.html", hint: "Proceed to Clue 02 location." },
+    clue2: { answers: ["bulbasaur"], next: "clue3.html", hint: "Proceed to Clue 03 location." },
+    clue3: { answers: ["caterpie"], next: "clue4.html", hint: "Proceed to Clue 04 location." },
+    clue4: { answers: ["rattata"], next: "clue5.html", hint: "Proceed to Clue 05 location." },
+    clue5: { answers: ["butterfree"], next: "clue6.html", hint: "Proceed to Clue 06 location." },
+    clue6: { answers: ["eevee"], next: "clue7.html", hint: "Proceed to Clue 07 location." },
+    clue7: { answers: ["moltres"], next: "../success.html", hint: "Path 1 Cleared!" }
+  },
+  path9: {
+    clue1: { answers: ["pikachu"], next: "clue2.html", hint: "Proceed to Clue 02 location." },
+    clue2: { answers: ["charmander"], next: "clue3.html", hint: "Proceed to Clue 03 location." },
+    clue3: { answers: ["squlrtle"], next: "clue4.html", hint: "Proceed to Clue 04 location." },
+    clue4: { answers: ["pidgey"], next: "clue5.html", hint: "Proceed to Clue 05 location." },
+    clue5: { answers: ["butterfree"], next: "clue6.html", hint: "Proceed to Clue 06 location." },
+    clue6: { answers: ["eevee"], next: "clue7.html", hint: "Proceed to Clue 07 location." },
+    clue7: { answers: ["chikorita"], next: "../success.html", hint: "Path 1 Cleared!" }
+  },
+  path10: {
+    clue1: { answers: ["pikachu"], next: "clue2.html", hint: "Proceed to Clue 02 location." },
+    clue2: { answers: ["charmander"], next: "clue3.html", hint: "Proceed to Clue 03 location." },
+    clue3: { answers: ["caterpie"], next: "clue4.html", hint: "Proceed to Clue 04 location." },
+    clue4: { answers: ["rattata"], next: "clue5.html", hint: "Proceed to Clue 05 location." },
+    clue5: { answers: ["butterfree"], next: "clue6.html", hint: "Proceed to Clue 06 location." },
+    clue6: { answers: ["eevee"], next: "clue7.html", hint: "Proceed to Clue 07 location." },
+    clue7: { answers: ["chikorita"], next: "../success.html", hint: "Path 1 Cleared!" }
   }
 };
 
